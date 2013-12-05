e1bf05c4d32ee1ddfd1b6045c1cf96f4e6aa6a16
Ricardo Gamba <rgamba@gmail.com>
2013-12-05 10:19:08 -0600

    Bug fixes

--- controller/default/nav/mod_meta.php
+++ controller/default/nav/mod_meta.php
@@ -2,8 +2,8 @@
 /**
 * Set metadata information
 */
-$data['META_DESCRIPTION'] = Tpl::get(META_DESCRIPTION);
-$data['META_KEYWORDS'] = Tpl::get(META_KEYWORDS);
+$data['META_DESCRIPTION'] = Tpl::get('META_DESCRIPTION');
+$data['META_KEYWORDS'] = Tpl::get('META_KEYWORDS');
 
 if($tpl)
     $tpl->setContext($data);
